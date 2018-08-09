20922190d5afdda9443eeb89f84c6ec25ea8f042
Florian Eck <it-support@friends-systems.de>
2018-08-09 17:28:29 +0200

    assign contact to projects

--- lib/easyredmine_helpdesk_self_signup/user_extension.rb
+++ lib/easyredmine_helpdesk_self_signup/user_extension.rb
@@ -4,7 +4,7 @@ module EasyredmineHelpdeskSelfSignup::UserExtension
   
   included do
     belongs_to :easy_contact
-    after_create :setup_easy_contact, :assign_to_helpdesk_projects
+    after_create :setup_easy_contact, :assign_to_helpdesk_projects, :assign_contact_to_projects
   end
   
   def easy_contact_by_users_email
@@ -12,6 +12,8 @@ module EasyredmineHelpdeskSelfSignup::UserExtension
     matching_email = CustomValue.find_by(custom_field_id: cf_id, value: self.email_address.address)
     matching_email.try(:customized)
   end
+  
+  private
   
   def setup_easy_contact
     return if self.email_address.nil?
@@ -22,8 +24,11 @@ module EasyredmineHelpdeskSelfSignup::UserExtension
     elsif ec.nil?
       cf_id = EasyContactCustomField.find_by_internal_name("easy_contacts_email").id
       
-      ec = EasyContact.new(firstname: firstname, lastname: lastname,
-      custom_field_values: {cf_id => self.email_address.address})
+      ec = EasyContact.new(
+        firstname: firstname, lastname: lastname,
+        custom_field_values: {cf_id => self.email_address.address},
+        type_id: 1, is_global: false
+      )
       
       ec.save
       self.update_column(:easy_contact_id, ec.id)
@@ -38,6 +43,13 @@ module EasyredmineHelpdeskSelfSignup::UserExtension
       member.role_ids = ([matching.easy_helpdesk_project.self_signup_default_role])
       member.save
     end
+  end
+  
+  def assign_contact_to_projects
+    projects.each do |project|
+      EasyContactEntityAssignment.create(easy_contact_id: self.easy_contact.id, entity: project)
+    end
+    
   end
   
 end
